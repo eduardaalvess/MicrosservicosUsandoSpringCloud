748c3301dd512892dcdc2636ac2091f2b076d7e7
saintravi <Dudah4@gmail.com>
2021-08-31 10:49:18 -0300

    first commit

---
+++ src/product.catalog/product.catalog/src/test/java/com/digitalinnovationone/experts/product/catalog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.experts.product.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
